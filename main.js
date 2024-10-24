7f6cb3e476945d8af53b1d3282501fe66b12634e
The_EnG1nE <baHHbaSh@gmail.com>
2024-10-24 13:22:42 +0500

    It's work

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+db = {
+    "inprodaction":"0.0 Введение",
+    "base": "1.0 Про html"
+}
 let data = document.querySelector("#data")
 let list = document.querySelector("#list")
+function getKeyByValue(object, value) {
+    return Object.entries(object).find(([key, val]) => val === value)[0];
+}
 function FromList(ArrayNames){
     let i = Array.from(JSON.parse(ArrayNames))
+    i = i.map(El=>db[El])
+    i.sort()
     let txt = "<p>Содержание</p>"
     i.forEach(Name=>{txt += `\n<a class="Link">${Name}</a>`})
     list.innerHTML = txt
@@ -10,7 +19,7 @@ function FromList(ArrayNames){
 function LinkNameToAction(){
     let Data = document.querySelectorAll(".Link")
     Data.forEach(Element=>{
-        Element.addEventListener("click", ()=>{LoadPage(Element.innerHTML)})
+        Element.addEventListener("click", ()=>{LoadPage(getKeyByValue(db, Element.innerHTML))})
     })
 }
 let LoadPage = (path) => {
